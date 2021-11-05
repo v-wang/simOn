17ad68fff726bb82bbd380590d276c37b6238dc0
v-wang <victorwang920@gmail.com>
2021-11-05 14:51:09 -0400

    refactored click event into separate function in preparation for settimeout, promises, setinterval

--- script.js
+++ script.js
@@ -63,51 +63,82 @@ console.log(simonStartColorSequence);
 
 // so we know when the user clicks on a piece, we can align that with the right color in the sequence
 let simonIndex = 0;
+function seqChecker(slice) {
+  let cPieceValue = slice.getAttribute("id");
+  // console.log(simonIndex);
+  // console.log(cPieceValue);
+  // console.log(simonStartColorSequence);
+
+  if (cPieceValue == simonStartColorSequence[simonIndex]) {
+    if (simonIndex == simonStartColorSequence.length - 1) {
+      console.log("nice, you won this round!");
+      sLength += 1;
+      // console.log(sLength);
+      simonStartColorSequence = numToColor(randomNum(sLength));
+      console.log(simonStartColorSequence);
+
+      return (simonIndex = 0);
+    }
+    return simonIndex++;
+  } else {
+    console.log("you got it wrong, game over");
+    sLength = 2;
+    // console.log((simonStartColorSequence = numToColor(randomNum(sLength))));
+    simonStartColorSequence = numToColor(randomNum(sLength));
+    console.log(simonStartColorSequence);
+    return (simonIndex = 0);
+  }
+}
 
 // click event added to each color piece for player functionality
 colorPieces.forEach((cPiece) => {
   cPiece.addEventListener("click", (event) => {
     event.preventDefault();
-
-    // test to see if color being captured
-    // console.log(cPiece.getAttribute("id"));
-
-    let cPieceValue = cPiece.getAttribute("id");
-    // console.log(simonIndex);
-    // console.log(cPieceValue);
-    // console.log(simonStartColorSequence);
-
-    if (cPieceValue == simonStartColorSequence[simonIndex]) {
-      if (simonIndex == simonStartColorSequence.length - 1) {
-        console.log("nice, you won this round!");
-        sLength += 1;
-        // console.log(sLength);
-        simonStartColorSequence = numToColor(randomNum(sLength));
-        console.log(simonStartColorSequence);
-
-        return (simonIndex = 0);
-      }
-      return simonIndex++;
-    } else {
-      console.log("you got it wrong, game over");
-      sLength = 2;
-      // console.log((simonStartColorSequence = numToColor(randomNum(sLength))));
-      simonStartColorSequence = numToColor(randomNum(sLength));
-      console.log(simonStartColorSequence);
-      return (simonIndex = 0);
-    }
-    // running into issue with app recognizing user input on new sequence (says its wrong even though it looks right in cl)
-
-    // console.log(simonIndex);
-    // console.log(cPieceValue);
-
-    // new color sequence needs to start here and shown to player
+    seqChecker(cPiece);
   });
 });
 
 let playerPassSample = ["purple", "red"];
 let playerFailSample = ["purple", "blue"];
 
-// function seqChecker(seq1, seq2, value) {
-//   for ()
-// }
+// LEGACY (SAVE FOR BACKUP)
+// // click event added to each color piece for player functionality
+// colorPieces.forEach((cPiece) => {
+//   cPiece.addEventListener("click", (event) => {
+//     event.preventDefault();
+
+//     // test to see if color being captured
+//     // console.log(cPiece.getAttribute("id"));
+
+//     let cPieceValue = cPiece.getAttribute("id");
+//     // console.log(simonIndex);
+//     // console.log(cPieceValue);
+//     // console.log(simonStartColorSequence);
+
+//     if (cPieceValue == simonStartColorSequence[simonIndex]) {
+//       if (simonIndex == simonStartColorSequence.length - 1) {
+//         console.log("nice, you won this round!");
+//         sLength += 1;
+//         // console.log(sLength);
+//         simonStartColorSequence = numToColor(randomNum(sLength));
+//         console.log(simonStartColorSequence);
+
+//         return (simonIndex = 0);
+//       }
+//       return simonIndex++;
+//     } else {
+//       console.log("you got it wrong, game over");
+//       sLength = 2;
+//       // console.log((simonStartColorSequence = numToColor(randomNum(sLength))));
+//       simonStartColorSequence = numToColor(randomNum(sLength));
+//       console.log(simonStartColorSequence);
+//       return (simonIndex = 0);
+//     }
+//     // running into issue with app recognizing user input on new sequence (says its wrong even though it looks right in cl)
+
+//     // console.log(simonIndex);
+//     // console.log(cPieceValue);
+
+//     // new color sequence needs to start here and shown to player
+//   });
+// });
