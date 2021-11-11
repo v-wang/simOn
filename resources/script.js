47ba3db947865fcac89be191ba5d080446d68edc
v-wang <victorwang920@gmail.com>
2021-11-10 21:12:06 -0500

    more styling fixes

--- resources/script.js
+++ resources/script.js
@@ -1,12 +1,13 @@
 // MOBILE DISAPPEARING BROWSER BAR
-const appHeight = () => {
-  document.documentElement.style.setProperty(
-    "--app-height",
-    `${window.innerHeight}px`
-  );
-};
-window.addEventListener("resize", appHeight);
-appHeight();
+
+let vh = window.innerHeight * 0.01;
+
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+window.addEventListener("resize", () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
 
 // DOM ELEMENTS
 
